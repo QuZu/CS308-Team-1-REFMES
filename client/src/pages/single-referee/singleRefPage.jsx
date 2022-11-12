7a2c3370046853883dede60fd90487f1dda2f7de
Enis Mert Kuzu <emertkuzu35@gmail.com>
2022-11-12 03:14:48 +0300

    add singleRef page

--- client/src/pages/single-referee/singleRefPage.jsx
+++ client/src/pages/single-referee/singleRefPage.jsx
@@ -1,15 +1,41 @@
 import React from "react";
 import AppNavBar from "../../components/appnavbar.jsx"
-
 import RefInfo from "../../components/refbar/refbar.jsx";
-
+import {useParams} from "react-router-dom"
+import axios from "axios";
+import { useEffect,useState } from "react";
+import * as ReactBootstrap from "react-bootstrap"
 function RefPage() {
-  var Refn="Mete Kalkavan"
+  const {refId} = useParams()
+  const [allData, setallData] = useState({});
+  const[loading,setLoading]=useState(false);
+
+  const getRef = async()=>{
+    await axios.get(`${process.env.REACT_APP_URL}/api/users/getref/${refId}`).then(res=>{
+      setallData(res.data);
+      setLoading(true);
+  
+    }).catch(err => console.log(err))
+  };
+  useEffect(() => {
+    getRef();
+  }, [])
+  console.log("Alldatam: ",allData)
     return (
       <div>
         <AppNavBar/>
-        <RefInfo refName={Refn}></RefInfo>
+        {loading && allData ?
+          <> 
+          <RefInfo refName={allData}></RefInfo>
+          
+          </>
+        :  
+        <div className="d-flex justify-content-center">
+           (<ReactBootstrap.Spinner animation="border"/>)
+        </div>
+        }
       </div>
+
     );
   }
   export default RefPage;
