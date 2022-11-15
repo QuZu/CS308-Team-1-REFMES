c4c93f6aba87d36fe99854aad3304eb09fb03e92
mkarabulut23 <mehmeteren.360@hotmail.com>
2022-11-15 17:32:46 +0300

    fix pre-match

--- client/src/pages/pre-match/pre-match.jsx
+++ client/src/pages/pre-match/pre-match.jsx
@@ -10,45 +10,45 @@ function PreMatchPage() {
         <div style={{textAlign:"center", margin: "2em", color: "blue"}}>
         <h1>Vote the Referees of the Week 17:</h1>
         </div>
-        <div style={{margin:"2em"}}>
-            <div className="row" style={{textAlign:"center",margin:"20px"}}>
-                <div className="col-6 vote_border" style={{backgroundColor:"lightblue", margin:"10px"}}>
+        <div style={{margin:" 2em auto",backgroundColor:"red"}}>
+            <div className="row" style={{textAlign:"center",margin:"auto"}}>
+                <div className="col-6 vote_border" style={{backgroundColor:"lightblue", marginTop:"10px", marginLeft:"100px"}}>
                     <p className="referee_name" style={{paddingTop:"10px"}}>Mete Kalkavan</p>
                 </div>
-                <div className="col-4 vote_border" style={{margin:"10px",backgroundColor:"lightblue"}}>
-                    <div className="user_vote_stars" style={{paddingTop:"10px"}}><BsStar id="1"/><BsStar id="2"/><BsStar id="3"/><BsStar id="4"/><BsStar id="5"/></div>
+                <div className="col-4 vote_border" style={{ marginTop:"10px", marginLeft:"10px",backgroundColor:"lightblue"}}>
+                    <div className="user_vote_stars" style={{paddingTop:"15px"}}><BsStar id="1"/><BsStar id="2"/><BsStar id="3"/><BsStar id="4"/><BsStar id="5"/></div>
                 </div>
             </div>
-            <div className="row" style={{textAlign:"center",margin:"20px"}}>
-                <div className="col-6 vote_border" style={{backgroundColor:"lightblue", margin:"10px"}}>
+            <div className="row" style={{textAlign:"center",margin:"auto"}}>
+                <div className="col-6 vote_border" style={{backgroundColor:"lightblue", marginTop:"10px", marginLeft:"100px"}}>
                     <p className="referee_name" style={{paddingTop:"10px"}}>Cüneyt Çakır</p>
                 </div>
-                <div className="col-4 vote_border" style={{margin:"10px",backgroundColor:"lightblue"}}>
-                    <div className="user_vote_stars" style={{paddingTop:"10px"}}><BsStar id="1"/><BsStar id="2"/><BsStar id="3"/><BsStar id="4"/><BsStar id="5"/></div>
+                <div className="col-4 vote_border" style={{marginTop:"10px", marginLeft:"10px",backgroundColor:"lightblue"}}>
+                    <div className="user_vote_stars" style={{paddingTop:"15px"}}><BsStar id="1"/><BsStar id="2"/><BsStar id="3"/><BsStar id="4"/><BsStar id="5"/></div>
                 </div>
             </div>
-            <div className="row" style={{textAlign:"center",margin:"20px"}}>
-                <div className="col-6 vote_border" style={{backgroundColor:"lightblue", margin:"10px"}}>
-                    <p  className="referee_name" style={{paddingTop:"10px"}}>Halil Umut Meler</p>
+            <div className="row" style={{textAlign:"center",margin:"auto"}}>
+                <div className="col-6 vote_border" style={{backgroundColor:"lightblue", marginTop:"10px", marginLeft:"100px"}}>
+                    <p className="referee_name" style={{paddingTop:"10px"}}>Halil Umut Meler</p>
                 </div>
-                <div className="col-4 vote_border" style={{margin:"10px",backgroundColor:"lightblue"}}>
-                    <div className="user_vote_stars" style={{paddingTop:"10px"}}><BsStar id="1"/><BsStar id="2"/><BsStar id="3"/><BsStar id="4"/><BsStar id="5"/></div>
+                <div className="col-4 vote_border" style={{marginTop:"10px", marginLeft:"10px",backgroundColor:"lightblue"}}>
+                    <div className="user_vote_stars" style={{paddingTop:"15px"}}><BsStar id="1"/><BsStar id="2"/><BsStar id="3"/><BsStar id="4"/><BsStar id="5"/></div>
                 </div>
             </div>
-            <div className="row" style={{textAlign:"center",margin:"20px"}}>
-                <div className="col-6 vote_border" style={{backgroundColor:"lightblue", margin:"10px"}}>
+            <div className="row" style={{textAlign:"center",margin:"auto"}}>
+                <div className="col-6 vote_border" style={{backgroundColor:"lightblue", marginTop:"10px", marginLeft:"100px"}}>
                     <p className="referee_name" style={{paddingTop:"10px"}}>Yaşar Kemal Uğurlu</p>
                 </div>
-                <div className="col-4 vote_border" style={{margin:"10px",backgroundColor:"lightblue"}}>
-                    <div className="user_vote_stars" style={{paddingTop:"10px"}}><BsStar id="1"/><BsStar id="2"/><BsStar id="3"/><BsStar id="4"/><BsStar id="5"/></div>
+                <div className="col-4 vote_border" style={{marginTop:"10px", marginLeft:"10px",backgroundColor:"lightblue"}}>
+                    <div className="user_vote_stars" style={{paddingTop:"15px"}}><BsStar id="1"/><BsStar id="2"/><BsStar id="3"/><BsStar id="4"/><BsStar id="5"/></div>
                 </div>
             </div>
-            <div className="row" style={{textAlign:"center",margin:"20px"}}>
-                <div className="col-6 vote_border" style={{backgroundColor:"lightblue", margin:"10px"}}>
+            <div className="row" style={{textAlign:"center",margin:"auto"}}>
+                <div className="col-6 vote_border" style={{backgroundColor:"lightblue", marginTop:"10px", marginLeft:"100px"}}>
                     <p className="referee_name" style={{paddingTop:"10px"}}>Hüseyin Göçek</p>
                 </div>
-                <div className="col-4 vote_border" style={{margin:"10px",backgroundColor:"lightblue"}}>
-                    <div className="user_vote_stars" style={{paddingTop:"10px"}}><BsStar style={{height: "50px"}} id="1"/><BsStar id="2"/><BsStar id="3"/><BsStar id="4"/><BsStar id="5"/></div>
+                <div className="col-4 vote_border" style={{marginTop:"10px", marginLeft:"10px",backgroundColor:"lightblue"}}>
+                    <div className="user_vote_stars" style={{paddingTop:"15px"}}><BsStar id="1"/><BsStar id="2"/><BsStar id="3"/><BsStar id="4"/><BsStar id="5"/></div>
                 </div>
             </div>
         </div>
