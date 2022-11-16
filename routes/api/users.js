879f0ebe10f7eb560ef6f9652e09a3c2d6143f53
Huseyin Alper Karadeniz <halperkk@gmail.com>
2022-11-16 22:55:34 +0300

    fix users api

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,27 +1,17 @@
 const express = require("express");
+const { response } = require("express");
 const router = express.Router();
 const bcrypt=require("bcrypt");
 require("dotenv").config();
+const User = require('../../models/userModel');
 
-const jwt=require("jsonwebtoken");
-// Item Model
-const User= require('../../models/usermodel');
-const { response } = require("express");
-const Ref = require("../../models/refereemodel");
-
-const Comment= require('../../models/commentModel');
-
-/**
- * @route   POST api/users
- * @desc    register new user
- * @access  Public
- */
-router.post('/register',async (req, res)=>{
-   const {name,identity,email,password} =req.body;
-   if(!name || !email || !password || !identity){
+router.post('/signup', async(req, res) => {
+  const {username, full_name, email, password, fan_of} = req.body;
+  if(!username || !full_name || !email || !password || !fan_of){
     return res.status(400).json({msg: "Please enter all fields"});
-   }
-   try {
+  }
+
+  try {
     const user = await User.findOne({ email });
     if (user) throw Error('User already exists');
 
@@ -31,92 +21,88 @@ router.post('/register',async (req, res)=>{
     const hash = await bcrypt.hash(password, salt);
     if (!hash) throw Error('Something went wrong hashing the password');
 
-    const newUser = new User({
-      name,
-      identity,
-      email,
-      password: hash
-    });
-
+    const newUser = new User({ username, full_name, email, password: hash, fan_of });
     const savedUser = await newUser.save();
-    if (!savedUser) throw Error('Something went wrong saving the user');
+    if (!savedUser) throw Error('Something went wrong while saving the user');
 
     res.status(200).json({
       user: {
         id: savedUser.id,
-        identity:savedUser.identity,
-        name: savedUser.name,
-        email: savedUser.email
-      }
-    });
-  } catch (e) {
-    res.status(400).json({ error: e.message });
-  }
-});
-router.post('/login', async (req, res) => {
-    const {email,password} =req.body;
-   if(!password || !email) {
-      return res.status(400).json({ msg: 'Please enter all fields' });
+        username: savedUser.username,
+        full_name: savedUser.full_name,
+        email: savedUser.email,
+        fan_of: savedUser.fan_of
+      }});
+
+    } catch (e) {
+      res.status(400).json({ error: e.message });
     }
+  }
+);
+
+router.post('/login', async(req, res) => {
+  const {email, password} = req.body;
+  if(!email || !password) {
+      return res.status(400).json({ msg: 'Please enter all fields' });
+  }
   
-    try {
-      // Check for existing user
-      const user = await User.findOne({ email });
-      if (!user) throw Error('User does not exist');
+  try {
+    const user = await User.findOne({ email });
+    if (!user) throw Error('User does not exist');
   
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) throw Error('Invalid credentials');
-  
-  
-      res.status(200).json({
-        user: {
-          id: user._id,
-          name: user.name,
-          email: user.email
-        }
-      });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw Error('Invalid credentials');
+
+    res.status(200).json({
+      user: {
+        id: user._id,
+        username: user.username,
+        full_name: user.full_name,
+        email: user.email,
+        fan_of: user.fan_of
+      }});
+
+    console.log(user);
+
     } catch (e) {
       res.status(400).json({ msg: e.message });
     }
-  });
-  router.post("/delete", async (req, res) => {
-    const {id,name,email} =req.body;
+  }
+);
+
+router.post("/delete", async(req, res) => {
+    const {id, username, email} =req.body;
     try {
       await User.findByIdAndDelete(id);
       res.status(200).json("User has been deleted...");
     } catch (err) {
       res.status(500).json(err);
     }
-  });
-  router.post("/updatesetting", async (req, res) => {
-    const dataID=req.body.id;
-      await User.findOneAndUpdate({email: req.body.email},
-        {name:req.body.name},
-       
-       );
-       console.log(`${dataID} User has been found...`)
-      res.status(200).json(`${dataID} User has been found...`);
-  });
- router.post("/change-password",async (req, res)=> {
-    
+  }
+);
+
+router.post("/updatesetting", async(req, res) => {
+    const dataID = req.body.id;
+      await User.findOneAndUpdate({email: req.body.email}, {name:req.body.name});
+      console.log(`User ${dataID} has been found...`);
+      res.status(200).json(`User ${dataID} has been found...`);
+  }
+);
+
+router.post("/change-password", async(req, res)=> {
     const BCRYPT_SALT_ROUNDS = 10;
     const currUser = req.body.user;
     let myQuery = { "email": currUser.user.email}
-    console.log("currentuser email:",currUser.user.email)
-    
-     
-    User
-    .findOne(myQuery, async (err, result) => {
-      if(err){
-        console.log(err.message)
-      }
-  
-      else if(result) {
-        console.log("result: ",result)
+    console.log("Current user email:", currUser.user.email)
+    User.findOne(myQuery, async (err, result) => {
+      if(err) {
+        console.log(err.message);
+      } else if(result) {
+        console.log("Result: ",result);
         const validPassword = await bcrypt.compare(req.body.currentPassword.toString(), result.password);
-        console.log(validPassword, ""+req.body.password, result.password)
+        console.log(validPassword, ""+req.body.password, result.password);
         if(validPassword){
-          console.log("res:",result)
+          console.log("Res:",result)
   
           bcrypt.genSalt(BCRYPT_SALT_ROUNDS, function (saltError, salt) {
           if (saltError) {
@@ -129,62 +115,23 @@ router.post('/login', async (req, res) => {
                 console.log(hashError);
                 return hashError
               }
-              console.log("updating...");
-               User.findOneAndUpdate({ "email": currUser.user.email }, 
-              {
-                  password: hash,
-                })
-                .then(() => {
-                  console.log('password changed');
+              console.log("Updating...");
+              User.findOneAndUpdate({"email": currUser.user.email}, {password: hash}).then(() => {
+                  console.log('Password changed');
                   res.status(200).json(hash);
                 });
             });
           }
           });
   
-        }else {
+        } else {
           res.status(200).json({message: "Current password is invalid"});
         }
-  
-      }
-      else {
+      } else {
         res.status(200).json({message: "Error! Please try again"});
       }
-  
-    }
-    );
-  });
-  
-router.get("/getref/:refName",async (req,res)=>{
-  const {name} =req.body;
-  try {
-    await Ref.findOne({name:req.params.refName}).then((result) => {
-
-      res.json(result);
-    })
-    .catch((err) => {
-      throw err;
     });
-  } catch (err) {
-    res.status(500).json(err);
   }
+);
 
-})
-  router.post("/sendComment", async (req, res) => {
-    const {userEmail, comment} = req.body;
-    console.log(userEmail);
-    console.log(comment);
-  });
-
-  router.post("/sendRating", async (req, res) => {
-    const {userEmail, rating, club1, club2, weekNo} = req.body;
-    console.log("\n");
-    console.log("Email: ", userEmail);
-    console.log("Rating: ", rating);
-    console.log("Club 1: ", club1);
-    console.log("Club 2: ", club2);
-    console.log("Week No: ", weekNo);
-    console.log("\n");
-  });
-  
 module.exports = router;
