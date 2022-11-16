c7956c704f1d228357698dade9b1f5cbaba4235f
Huseyin Alper Karadeniz <halperkk@gmail.com>
2022-11-16 22:53:41 +0300

    fix comment model

--- models/commentModel.js
+++ models/commentModel.js
@@ -1,17 +1,29 @@
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-  userEmail:{
-    type:String,
-    required:true
-  },
-  comment: {
+  comment:{
     type: String,
+    required: true
+  },
+  user_id: {
+    type: String,
+    required: true
+  },
+  match_id:{
+    type: String,
+    required: true
+  },
+  referee_id:{
+    type: String,
+    required: true
+  },
+  date:{
+    type: Date,
     required: true
   }
 });
 
-const Comment = mongoose.model('comment', CommentSchema);
+const Comment = mongoose.model('comments', CommentSchema);
 
 module.exports = Comment
