dc850b8b5975f99240352c2b2dc657a97515d719
Huseyin Alper Karadeniz <halperkk@gmail.com>
2022-11-16 22:53:46 +0300

    add match model

---
+++ models/matchModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MatchSchema = new Schema({
+  week_no:{
+    type: String,
+    required: true
+  },
+  referee_id: {
+    type: String,
+    required: true
+  },
+  club1_id:{
+    type: String,
+    required: true
+  },
+  club2_id:{
+    type: String,
+    required: true
+  },
+  club1_goals:{
+    type: String,
+    required: true
+  },
+  club2_goals:{
+    type: String,
+    required: true
+  }
+});
+
+const Match = mongoose.model('matches', MatchSchema);
+
+module.exports = Match
