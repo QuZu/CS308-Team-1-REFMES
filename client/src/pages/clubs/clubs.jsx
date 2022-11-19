68d59717f8510f2aeba6e2a4000770e653049f16
Ahmet Bilal Yildiz <71377609+abilals58@users.noreply.github.com>
2022-11-19 20:22:57 +0300

    add logos to single club page

--- client/src/pages/clubs/clubs.jsx
+++ client/src/pages/clubs/clubs.jsx
@@ -9,6 +9,23 @@ function ClubsPage() {
     var club1="fenerbahce";
     var club2="galatasaray";
     var club3="besiktas";
+    var club4="basaksehirspor";
+    var club5="adanademirspor";
+    var club6="konyaspor";
+    var club7="hatayspor";
+    var club8="giresunspor";
+    var club9="alanyaspor";
+    var club10="sivasspor";
+    var club11="antalyaspor";
+    var club12="gaziantepfk";
+    var club13="umraniyespor";
+    var club14="istanbulspor";
+    var club15="kasimpasaspor";
+    var club16="ankaragucuspor";
+    var club17="trabzonspor";
+    var club18="karagumrukspor";
+    var club19="kayserispor";
+
 
     return(
         <div>
@@ -21,6 +38,22 @@ function ClubsPage() {
                   <div className="edit_buttons_inner"><a href={`/club/${club1}`}>{club1}</a></div>
                   <div className="edit_buttons_inner"><a href={`/club/${club2}`}>{club2}</a></div>
                   <div className="edit_buttons_inner"><a href={`/club/${club3}`}>{club3}</a></div>
+                  <div className="edit_buttons_inner"><a href={`/club/${club4}`}>{club4}</a></div>
+                  <div className="edit_buttons_inner"><a href={`/club/${club5}`}>{club5}</a></div>
+                  <div className="edit_buttons_inner"><a href={`/club/${club6}`}>{club6}</a></div>
+                  <div className="edit_buttons_inner"><a href={`/club/${club7}`}>{club7}</a></div>
+                  <div className="edit_buttons_inner"><a href={`/club/${club8}`}>{club8}</a></div>
+                  <div className="edit_buttons_inner"><a href={`/club/${club9}`}>{club9}</a></div>
+                  <div className="edit_buttons_inner"><a href={`/club/${club10}`}>{club10}</a></div>
+                  <div className="edit_buttons_inner"><a href={`/club/${club11}`}>{club11}</a></div>
+                  <div className="edit_buttons_inner"><a href={`/club/${club12}`}>{club12}</a></div>
+                  <div className="edit_buttons_inner"><a href={`/club/${club13}`}>{club13}</a></div>
+                  <div className="edit_buttons_inner"><a href={`/club/${club14}`}>{club14}</a></div>
+                  <div className="edit_buttons_inner"><a href={`/club/${club15}`}>{club15}</a></div>
+                  <div className="edit_buttons_inner"><a href={`/club/${club16}`}>{club16}</a></div>
+                  <div className="edit_buttons_inner"><a href={`/club/${club17}`}>{club17}</a></div>
+                  <div className="edit_buttons_inner"><a href={`/club/${club18}`}>{club18}</a></div>
+                  <div className="edit_buttons_inner"><a href={`/club/${club19}`}>{club19}</a></div>
               </div>
             </div>
           </div>
--- client/src/pages/single-club/single-club.jsx
+++ client/src/pages/single-club/single-club.jsx
@@ -5,13 +5,35 @@ import { useParams } from "react-router-dom";
 import React, { useCallback, useState } from "react";
 import axios from "axios";
 import "./single-club.css" 
-import clubLogo from '../../logos/fenerbahce.png';
 import { useEffect } from "react";
 
-function SingleClubPage() {
+
+//importing all club logos
+import logoFenerbahce from '../../logos/fenerbahce.png';
+import logoGalatasaray from '../../logos/galatasaray.png';
+import logoBesiktas from '../../logos/besiktas.png';
+import logoBasaksehir from '../../logos/basaksehir.png';
+import logoAdanaDemirspor from '../../logos/adana_demirspor.png';
+import logoKonyaspor from '../../logos/konyaspor.png';
+import logoHatayspor from '../../logos/hatayspor.png';
+import logoGiresunspor from '../../logos/giresunspor.png';
+import logoAlanyaspor from '../../logos/alanyaspor.png';
+import logoSivasspor from '../../logos/sivasspor.png';
+import logoAntalyaspor from '../../logos/antalyaspor.png';
+import logoGaziantepFK from '../../logos/gaziantep_fk.png';
+import logoUmraniyespor from '../../logos/umraniyespor.png';
+import logoIstanbulspor from '../../logos/istanbulspor.png';
+import logoKasimpasa from '../../logos/kasimpasa.png';
+import logoAnkaragucu from '../../logos/ankaragucu.png';
+import logoTrabzonspor from '../../logos/trabzonspor.png';
+import logoKaragumruk from '../../logos/karagumruk.png';
+import logoKayserispor from '../../logos/kayserispor.png';
+
+
+function SingleClubPage() { // it takes clubname parameter from clubs.jsx
   // getting parameters from clubs page
   const{clubName} = useParams();
-  console.log("clubname: " , clubName);
+  console.log("clubname use params: " , clubName);
   const [ClubData, setClubData] = useState({});
   const [Loading, setLoading] = useState(false);
     
@@ -25,11 +47,38 @@ function SingleClubPage() {
 
   };
 
+
   useEffect(()=> {
     getClub();
   }, [])
   console.log(ClubData);
+  // declaring the logos dictionary
+  const images = [
+    { id: "fenerbahce", src: logoFenerbahce},
+    { id: "galatasaray", src: logoGalatasaray},
+    { id: "besiktas", src: logoBesiktas},
+    { id: "basaksehirspor", src: logoBasaksehir},
+    { id: "adanademirspor", src: logoAdanaDemirspor},
+    { id: "konyaspor", src: logoKonyaspor},
+    { id: "hatayspor", src: logoHatayspor},
+    { id: "giresunspor", src: logoGiresunspor},
+    { id: "alanyaspor", src: logoAlanyaspor},
+    { id: "sivasspor", src: logoSivasspor},
+    { id: "antalyaspor", src: logoAntalyaspor},
+    { id: "gaziantepfk", src: logoGaziantepFK},
+    { id: "umraniyespor", src: logoUmraniyespor},
+    { id: "istanbulspor", src: logoIstanbulspor},
+    { id: "kasimpasaspor", src: logoKasimpasa},
+    { id: "ankaragucuspor", src: logoAnkaragucu},
+    { id: "trabzonspor", src: logoTrabzonspor},
+    { id: "karagumrukspor", src: logoKaragumruk},
+    { id: "kayserispor", src: logoKayserispor}
 
+  ]
+
+  // result = related image source
+  const result=(images.find(({id})=>id === clubName)).src;
+  console.log("result: ", result);
 
   return(
       
@@ -37,12 +86,12 @@ function SingleClubPage() {
         <AppNavBar/>
 
         <div className = "club-general-info">
-          <div className = "club-logo"> <a href = "https://www.fenerbahce.org/" target = "_blank"> <img id = "club-image" src = {clubLogo} alt = "Fenerbahce logo" /> </a> </div>
+          <div className = "club-logo"> <a href = {ClubData.website} target = "_blank"> <img id = "club-image" src = {result} alt = "Fenerbahce logo" /> </a> </div>
           <div className = "club-info">
-            <div className = "c-i-h"> <h1 id = "c-info-head"> Fenerbahçe Spor Kulübü</h1> <br/> </div>
-            <div className = "c-i-t1"> <p className="c-info-text"> Average age: 25</p> <br/> </div>
-            <div className = "c-i-t2"> <p className="c-info-text"> Stadium informations: Ülker Fenerbahçe Şükrü Saracoğlu Stadyumu</p> </div>
-            <div className = "c-i-g"> <p className="c-info-text"> Fenerbahçe Spor Kulübü (1907)</p> 
+            <div className = "c-i-h"> <h1 id = "c-info-head"> {ClubData.full_name} </h1> <br/> </div>
+            <div className = "c-i-t1"> <p className="c-info-text"> Founded: {ClubData.founded}</p> <br/> </div>
+            <div className = "c-i-t2"> <p className="c-info-text"> Stadium informations: {ClubData.stadium}</p> </div>
+            <div className = "c-i-g"> <p className="c-info-text"> {ClubData.full_name} ({ClubData.founded}) </p> 
             <p className="c-info-text">Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum
             Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstumLorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum
             Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstumLorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum Lorem-impstum
