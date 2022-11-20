19d9f2115ff63e49f1432403cc8185d66b117713
Enis Mert Kuzu <emertkuzu35@gmail.com>
2022-11-21 02:49:28 +0300

    add comment box

--- client/src/components/comment/commentbox.jsx
+++ client/src/components/comment/commentbox.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../comment/commentbox.css"
-function Comment({ commentPerson,pComment }) {
+function Comment({ commentPerson,pComment,Date }) {
     return(
         <div className="col-12">
         <div className="container d-flex justify-content-start">
@@ -8,13 +8,15 @@ function Comment({ commentPerson,pComment }) {
             <div className="post-heading">
                 <div className="float-left meta">
                     <div className="title h5">
-                      <a href="/aa"><b>{commentPerson}</b></a>
+                      <a className="px-2">user: <b>{commentPerson}</b></a>
                     </div>
-                    <h6 className="text-muted time">1 minute ago</h6>
+                    <h6 className="px-2 text-muted time">Date: {Date.substring(0,10)}</h6>
                 </div>
             </div> 
-            <div className="post-description"> 
+            <div className="post-description">
+            <div className="card-heading px-2 pt-2 pb-1">
               <p>{pComment}</p>
+              </div>
             </div>
           </div>
         </div>
