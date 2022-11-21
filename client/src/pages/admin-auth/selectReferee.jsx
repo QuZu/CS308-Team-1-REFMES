09759e30ac0f49898d766d62608caf7d32772d03
mkarabulut23 <mehmeteren.360@hotmail.com>
2022-11-21 20:10:57 +0300

    add selectReferee page

---
+++ client/src/pages/admin-auth/selectReferee.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function AdminSelectReferee(){
+    return(
+        <h1> BU HAFTA MAÇ YÖNETECEK HAKEMLER BU SAYFADAN SEÇİLECEK</h1>
+    )
+};
+export default AdminSelectReferee;
