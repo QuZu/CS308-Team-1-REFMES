ed9e2fd5b1e532febc96371feda5fb1a08e80817
Ahmet Bilal Yildiz <71377609+abilals58@users.noreply.github.com>
2022-11-21 19:58:23 +0300

    update clubs.jsx

--- client/src/pages/clubs/clubs.jsx
+++ client/src/pages/clubs/clubs.jsx
@@ -1,63 +1,45 @@
 import { useStore } from "../../store/store";
 import AppNavBar from "../../components/appnavbar.jsx";
-import { useNavigate } from "react-router";
-import React, { useCallback, useState } from "react";
+import React, {  useEffect, useState } from "react";
 import axios from "axios";
+import ClubCard from "../../components/clubs/clubCard"
 
 function ClubsPage() {
 
-    var club1="fenerbahce";
-    var club2="galatasaray";
-    var club3="besiktas";
-    var club4="basaksehirspor";
-    var club5="adanademirspor";
-    var club6="konyaspor";
-    var club7="hatayspor";
-    var club8="giresunspor";
-    var club9="alanyaspor";
-    var club10="sivasspor";
-    var club11="antalyaspor";
-    var club12="gaziantepfk";
-    var club13="umraniyespor";
-    var club14="istanbulspor";
-    var club15="kasimpasaspor";
-    var club16="ankaragucuspor";
-    var club17="trabzonspor";
-    var club18="karagumrukspor";
-    var club19="kayserispor";
+  const [ClubsData, setClubData] = useState({});
+  const [Loading, setLoading] = useState(false);
 
+  const getClubs = async() =>{
+    await axios.get(`${process.env.REACT_APP_URL}/api/clubs/getClubs`).then(response =>{
+      console.log("response: ", response);
+      setClubData(response.data);
+      setLoading(true);
 
+    }).catch(err => console.log(err))
+
+  };
+
+  useEffect(()=> {
+    getClubs();
+  }, [])
+  console.log(ClubsData);
     return(
         <div>
             <AppNavBar/>
-            <h1 style={{textAlign: "center", marginTop: "172px"}}>Clubs Page</h1>
-            <div className="edit_buttons_main">
-          <div className="card p-1 mb-0 card-shadow change-password">
-            <div className="card-body">
-                <div className="row edit_buttons_container">
-                  <div className="edit_buttons_inner"><a href={`/club/${club1}`}>{club1}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club2}`}>{club2}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club3}`}>{club3}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club4}`}>{club4}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club5}`}>{club5}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club6}`}>{club6}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club7}`}>{club7}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club8}`}>{club8}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club9}`}>{club9}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club10}`}>{club10}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club11}`}>{club11}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club12}`}>{club12}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club13}`}>{club13}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club14}`}>{club14}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club15}`}>{club15}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club16}`}>{club16}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club17}`}>{club17}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club18}`}>{club18}</a></div>
-                  <div className="edit_buttons_inner"><a href={`/club/${club19}`}>{club19}</a></div>
-              </div>
+            <div>
+            { ClubsData ?
+                    (ClubsData.length > 0 ?
+                      ClubsData.map((item) => {
+                        
+                        return(
+                          <div key={item.name}> 
+                            <ClubCard clubName = {item.name} asciName = {item.asci_name}>  </ClubCard>
+                          </div>
+                        );
+                      }) : <></>)            :<></>
+                   
+                  }
             </div>
-          </div>
-        </div>
         </div>
     )
 }
