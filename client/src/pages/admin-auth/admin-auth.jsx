319c180d6021c5767576fd0f6ecd2644265ee198
mkarabulut23 <mehmeteren.360@hotmail.com>
2022-12-04 15:34:22 +0300

    fix admin-auth page

--- client/src/pages/admin-auth/admin-auth.jsx
+++ client/src/pages/admin-auth/admin-auth.jsx
@@ -10,6 +10,12 @@ function AdminAuthPage(){
       const goToSelectReferee = (e) => {
         navigate("/admin-auth/selectReferee")
       }
+      const goToAddObserver = (e) => {
+        navigate("/admin-auth/addObserver")
+      }
+      const goToEnterResults = (e) => {
+        navigate("/admin-auth/enterResult")
+      }
     return(
         <div>
             <div> <img src={logo} style={{height: "150px", marginLeft:"500px"}} alt="refmes_logo"/> </div>
@@ -18,7 +24,13 @@ function AdminAuthPage(){
                     <button onClick={goToAddReferee} style={{margin: "0 auto"}} type='submit' className="col-5 btn btn-block btn-success">
                     ADD REFEREE</button>
                     <button onClick={goToSelectReferee} style={{margin: "0 auto"}} type='submit' className="col-5 btn btn-block btn-warning">
-                    SELECT THE REFEREE OF THE WEEK</button>
+                    SELECT THE REFEREES OF THE WEEK</button>
+            </div>
+            <div style={{minHeight:"100px", marginTop:"20px"}}  className="row">
+                    <button onClick={goToAddObserver} style={{margin: "0 auto"}} type='submit' className="col-5 btn btn-block btn-success">
+                    ADD OBSERVER</button>
+                    <button onClick={goToEnterResults} style={{margin: "0 auto"}} type='submit' className="col-5 btn btn-block btn-warning">
+                   ENTER THE RESULTS OF THE MATCHES</button>
             </div>
         </div>
     );
