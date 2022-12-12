81325ccd77ccb47cba2991291b2125a62dd119ba
Huseyin Alper Karadeniz <halperkk@gmail.com>
2022-12-12 22:49:05 +0300

    Update matches page main structure
    Updated the main structure of the matches page
    by adding a new match details component

--- client/src/pages/matches/matches.jsx
+++ client/src/pages/matches/matches.jsx
@@ -1,14 +1,45 @@
 import { useStore } from "../../store/store";
 import AppNavBar from "../../components/appnavbar.jsx";
 import { useNavigate } from "react-router";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import axios from "axios";
+import "../matches/matches.css";
+import * as ReactBootstrap from "react-bootstrap";
 
 function MatchesPage() {
+    const weekNo = "3";
+
+    const [matchesData, setMatchesData] = useState({});
+    const [loading, setLoading] = useState(false);
+
+    const getMatchDetails = async() => {
+        await axios
+            .get(`${process.env.REACT_APP_URL}/api/matches/getMatchDetails/${weekNo}`)
+            .then(res => {
+                setMatchesData(res.data);
+                setLoading(true);
+        }).catch(err => console.log(err));
+    };
+
+    useEffect(() => {
+        getMatchDetails();
+    }, []);
+
     return(
         <div>
             <AppNavBar/>
-            <h1 style={{textAlign: "center", marginTop: "172px"}}>Matches Page</h1>
+            <div><h1 style={{textAlign: "center", marginTop: "172px"}}>Matches Page</h1></div>
+            {loading && matchesData ?
+                <div className="matches">
+                {matchesData.map((singleMatchDetails) => {
+                    return(<div key={singleMatchDetails._id} matchData={singleMatchDetails}>{singleMatchDetails._id}</div>)
+                })}
+                </div>
+                :
+                <div className="d-flex justify-content-center">
+                    <ReactBootstrap.Spinner animation="border"/>
+                </div>
+            }
         </div>
     )
 }
