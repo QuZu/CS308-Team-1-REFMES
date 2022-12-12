6875da2e0ca06970d76278c48a80085abc25293c
mkarabulut23 <mehmeteren.360@hotmail.com>
2022-12-12 22:19:35 +0300

    add necessary functions to selectReferee.jsx

--- client/src/pages/admin-auth/selectReferee.jsx
+++ client/src/pages/admin-auth/selectReferee.jsx
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import RefCard from "../../components/refbar/refcard";
 import "../admin-auth/selectReferee.css";
-function AdminSelectReferee(){
-    const [allData, setallData] = useState({});
-  const[loading,setLoading] = useState(false);
-
-  const getAllRef = async()=>{
-    await axios.get(`${process.env.REACT_APP_URL}/api/referees/getAllref`).then(res=>{
-      setallData(res.data);
-      setLoading(true);
+function AdminSelectReferee({currentweek,allData}){
+  const [resultMessage, setResultMessage] = useState("");
+  console.log(allData);
   
-    }).catch(err => console.log(err))
-  };
-  useEffect(() => {
-    getAllRef();
-  }, [])
-    console.log(allData);
+      // With this useState I wan't to collect the checked checkboxes
+      const [checkedCheckboxes, setCheckedCheckboxes] = useState([]);
+    
+      // This is my handler method that gets triggered when a checkbox get's checked/unchecked
+      // ..and toggles the state of the checkbox
+      const handleCheckboxChange = (data) => {
+        const isChecked = checkedCheckboxes.some(checkedCheckbox => checkedCheckbox.name === data.name)
+        if (isChecked) {
+          setCheckedCheckboxes(
+            checkedCheckboxes.filter(
+              (checkedCheckbox) => checkedCheckbox.name !== data.name
+            )
+          );
+        } else {
+          setCheckedCheckboxes(checkedCheckboxes.concat(data));
+        }
+      };
+
+
+      useEffect(() => {
+        console.log(checkedCheckboxes);
+    }, [checkedCheckboxes]);
+    //console.log(allData);
     function compare( a, b ) {
       if ( a.name < b.name ){
         return -1;
@@ -26,27 +37,44 @@ function AdminSelectReferee(){
       }
       return 0;
     }
-  if(loading)
-  {
-    allData.sort(compare);
+  allData.sort(compare);
+
+  const handleSubmit = async(e) =>{
+    e.preventDefault();
+    console.log(checkedCheckboxes);
+    const newRefereesOfWeek ={
+      week_no: currentweek,
+      referees: checkedCheckboxes
+    };
+    if(checkedCheckboxes.length !== 9){
+      setResultMessage("Please, select exactly 9 referees!");
+    }
+    else{
+      setResultMessage("You have selected the referee list successfully!");
+    }
+    await axios.post(`${process.env.REACT_APP_URL}/api/admin/selectReferee`,newRefereesOfWeek)
+    .then(res =>{
+        console.log(res.data);
+
+    }).catch(err=>console.log(err));
   }
-    
     return(
         <div>
-             <h1 className="selectRefh1"> SELECT THE REFEREES OF THE WEEK 14: </h1>
-             <form className="selectRefform">
+             <form  onSubmit={handleSubmit}  className="selectRefform">
              <div className="mt-3 container">
                 <div className="row">
              { allData ?
             (allData.length > 0 ?
               allData.map((item) => {
-          
 
                 return(
-                  <div key={item.r_username} className="col-xl-3 col-sm-6 mb-5">
-                    <label>
-                        <input type="checkbox"/> <RefCard r_username={item.r_username} Refname={item.name}></RefCard>
-                    </label>
+                  <div key={item.name} className="col-xl-3 col-sm-6 mb-5">
+                    
+                        <input
+                        value={item.name}
+                        type="checkbox"
+                        checked={checkedCheckboxes.some(checkedCheckbox => checkedCheckbox.name === item.name)}
+                        onChange={() => handleCheckboxChange(item)}/>{item.name}
                   </div>
 
                 );
@@ -55,8 +83,9 @@ function AdminSelectReferee(){
           }
                 </div>
             </div>
+            <p className="selectRefereeResultMessage">{resultMessage}</p>
             <label>
-            <button  style={{marginLeft: "500px"}} type="submit" className="btn btn-block col-8 btn-success">CONFIRM THE LIST</button>
+            <button style={{marginLeft: "500px"}} type="submit" className="btn btn-block col-8 btn-success">CONFIRM THE LIST</button>
             </label>
           </form>
         </div>
