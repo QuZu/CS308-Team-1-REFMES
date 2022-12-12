7ec9376488f3163aeaf3ead6e218a40de6bb7be9
mkarabulut23 <mehmeteren.360@hotmail.com>
2022-12-12 22:23:23 +0300

    selectReferee send updates to the database

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -5,6 +5,8 @@ const bcrypt=require("bcrypt");
 require("dotenv").config();
 const Referee = require('../../models/refereeModel');
 const Observer = require('../../models/observerModel');
+const Match = require('../../models/matchModel');
+const TrialRefereesOfWeek = require('../../models/trialrefereesOfWeekModel');
 
 router.post('/addReferee', async(req, res) => {
     console.log("Gelen datam:", req.body); 
@@ -67,6 +69,40 @@ router.post('/addReferee', async(req, res) => {
         observer: {
           observer_id: savedObserver.observer_id,
           password: savedObserver.password,
+        }});
+  
+      } catch (e) {
+        res.status(400).json({ error: e.message });
+      }
+    }
+  );
+  router.post('/updateMatchScore', async(req, res) => {
+    const {match_id,team1goal,team2goal}= req.body;
+    console.log(match_id);
+    console.log("Gelen datam:", req.body); 
+    await Match.findByIdAndUpdate(match_id,{club1_goals:team1goal, club2goals: team2goal}).then(() => {
+      res.status(200);
+    }
+      
+    )
+  }
+  );
+  router.post('/selectReferee', async(req, res) => {
+    console.log("Gelen datam:", req.body); 
+  const {week_no,referees} = req.body;
+    if(referees.length !== 9) {
+      return res.status(400).json({msg: "Please select exactly 9 referees!"});
+    }
+  
+    try {
+      const newRefereesOFWeek = new TrialRefereesOfWeek({week_no,referees});
+      const savedRefereesOFWeek = await newRefereesOFWeek.save();
+      if (!savedRefereesOFWeek) throw Error('Something went wrong while saving the referee list');
+  
+      res.status(200).json({
+        refereesOfWeek: {
+          week_no: savedRefereesOFWeek.week_no,
+          referees: savedRefereesOFWeek.referees,
         }});
   
       } catch (e) {
