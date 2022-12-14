b285da7d56fd340cacba13a8915476f4c9b2f8f6
Huseyin Alper Karadeniz <halperkk@gmail.com>
2022-12-14 21:38:12 +0300

    Update match box component
    Updated match box component, so that it has a better functioning.

--- client/src/components/matchbox/matchbox.jsx
+++ client/src/components/matchbox/matchbox.jsx
@@ -47,102 +47,60 @@ const clubs = [
     { name: "Kayserispor", src: logoKayserispor},
   ]
 
-function MatchBox({ matchData }) {
+function MatchBox({ matchData, playedWeek }) {
 
     const [state, dispatch] = useStore();
     const {user:currentUser} = state;
-    const [matchPlayed, setMatchPlayed] = useState(true);
-
-
-
-    const [rating, setRating] = useState(0);
-    const [errorMessage, setErrorMessage] = useState("");
-    const [isInteractive, setIsInteractive] = useState(true);
-    const [btnValue, setBtnValue] = useState("Submit");
-    const [ratingEntered, setRatingEntered] = useState(true);
-    const [btnDisabled, setBtnDisabled] = useState(false);
-    const [day, setDay] = useState("");
-    const [month, setMonth] = useState("");
-    const [year, setYear] = useState("");
-
-    const monthSet = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (rating == 0) {
-            setRatingEntered(false);
-        } else {
-            setRatingEntered(true);
-            const newPostRating = {rating: rating, user_id: currentUser.user.id, match_id: matchData._id};
-            axios
-                .post(`${process.env.REACT_APP_URL}/api/postRatings/addPostRating`, newPostRating)
-                .then((res) => {
-                    if (res.status === 200 && res.data.message) {
-                        setErrorMessage(res.data.message);
-                    } else if (res.status === 200) {
-                        setErrorMessage("Your rating submitted successfully");
-                    } else {
-                        setErrorMessage("Error! Please try again.");
-                    }
-                }).catch((err) => {
-                    console.log("Error: ", err);
-                    setErrorMessage("Error! Please try again.");
-                });
-            const date = new Date();
-            setDay(date.getDate());
-            setMonth(monthSet[date.getMonth()]);
-            setYear(date.getFullYear());
-            setIsInteractive(false);
-            setBtnValue("Saved");
-            setBtnDisabled(true);
-        }
-    }
-
-    const getCurrentPostRating = async()=>{
-        await axios.get(`${process.env.REACT_APP_URL}/api/postRatings/getPostRating/${matchData._id}/${currentUser.user.id}`).then(res => {
-            setRating(res.data);
-            if (res.data == []) {
-                console.log("Empty");
-            } else {
-                setRating(res.data[0].rating);
-                const date = new Date(res.data[0].date);
-                setDay(date.getDate());
-                setMonth(monthSet[date.getMonth()]);
-                setYear(date.getFullYear());
-                setIsInteractive(false);
-                setBtnValue("Saved");
-                setBtnDisabled(true);
-            }
-        }).catch(err => console.log(err))
-    };
-
-    useEffect(() => {
-        getCurrentPostRating();
-    }, []);
+    const [hasRefInfo, setHasRefInfo] = useState(matchData.referee_id ? true : false);
 
     return (
         <>
         <div className="matchbox-outer-container">
             <div className="matchbox-inner-container">
-                {matchPlayed ?
-                    <div className="matchbox-inner-left">
+                <div className="matchbox-inner-left">
 
+                    <div className="matchbox-inner-left-name">
                         <a>{matchData.club1_info[0].name}</a>
-                        <img className="matchbox-left-club-img" src={(clubs.find(({name})=>name == matchData.club1_info[0].name)).src}/>
-                        <img className="matchbox-left-club-img" src={(clubs.find(({name})=>name == matchData.club2_info[0].name)).src}/>
+                    </div>
+
+                    <div className="matchbox-inner-left-image">
+                        <img className="matchbox-inner-left-club-img" src={(clubs.find(({name})=>name == matchData.club1_info[0].name)).src}/>
+                    </div>
+
+                    <div className="matchbox-inner-left-score">
+                        {playedWeek ? <a>{matchData.club1_goals}</a> : <a>-</a>}
+                        
+                    </div>
+
+                </div>
+                <div className="matchbox-inner-right">
+
+                    <div className="matchbox-inner-right-score">
+                        {playedWeek ? <a>{matchData.club2_goals}</a> : <a>-</a>}
+                    </div>
+
+                    <div className="matchbox-inner-right-image">
+                        <img className="matchbox-inner-right-club-img" src={(clubs.find(({name})=>name == matchData.club2_info[0].name)).src}/>
+                    </div>
+
+                    <div className="matchbox-inner-right-name">
                         <a>{matchData.club2_info[0].name}</a>
-
                     </div>
-                :
-                    <div className="matchbox-left">
 
-                        <img src={(clubs.find(({name})=>name == matchData.club1_info[0].name)).src}/>
-                        <img src={(clubs.find(({name})=>name == matchData.club2_info[0].name)).src}/>
-
-                    </div>
-                }
-                <div className="matchbox-right">
-
+                </div>
+            </div>
+            <div className="matchbox-middle-container">
+                <div className="matchbox-middle-referee">
+                    {playedWeek && hasRefInfo ?
+                        <div><b>Referee:</b> <Link to={`../referee/${matchData.ref_info[0].r_username}`}>{matchData.ref_info[0].name}</Link></div>
+                        :
+                        <a>Referee is not assigned yet.</a>
+                    }
+                </div>
+            </div>
+            <div className="matchbox-bottom-container">
+                <div className="matchbox-bottom-button">
+                    <Link to={`../match/${matchData._id}`}>See Match Details</Link>
                 </div>
             </div>
         </div>
@@ -150,37 +108,3 @@ function MatchBox({ matchData }) {
     );
   }
   export default MatchBox;
-
-  /*
-        <div className="rating-outer-container">
-            <div className="rating-container">
-                <div className="rating-left">
-                    <div className="rating-left-match">
-                        <div className="rating-team">
-                            <img src={(clubs.find(({name})=>name == matchData.club1_info[0].name)).src}/>
-                            <a>{matchData.club1_info[0].name} <b>({matchData.club1_goals})</b></a>
-                        </div>
-                        <a> vs. </a>
-                        <div className="rating-team">
-                            <img src={(clubs.find(({name})=>name == matchData.club2_info[0].name)).src}/>
-                            <a>{matchData.club2_info[0].name} <b>({matchData.club2_goals})</b></a>
-                        </div>
-                    </div>
-                </div>
-                <div className="rating-right">
-                    <div className="rating-right-referee"><a href={`../referee/${matchData.ref_info[0].r_username}`}><b>{matchData.ref_info[0].name}</b></a></div>
-                    <Rater onRate={({rating}) => {setRating(rating); setRatingEntered(true);}} total={5} rating={rating} interactive={isInteractive}/>
-                    {isInteractive ? <></> : <div className="rating-right-date">{month} {day}, {year}</div>}
-                    {ratingEntered ? <></> : <div className="rating-right-error"><a>Choose a rating, please!</a><br/></div>}
-                </div>
-                <div className="rating-submit">
-                <form onSubmit={handleSubmit}>
-                    <input type="submit" name="submitButton" disabled={btnDisabled} className="btn btn-success" value={`${btnValue}`}/>
-                </form>
-                </div>
-            </div>
-            <div className="rating-comment-container">
-                <div className="rating-comment-add-button"><Link to={`../matches/${matchData._id}`}>Add Comment</Link></div>
-            </div>
-        </div>
-  */
