8d8322f2234daa00ae15d4151a30aafc0c5b1bc8
Huseyin Alper Karadeniz <halperkk@gmail.com>
2022-12-22 09:06:07 +0300

    Fix club page alignment issues
    Updated the alignment of the card components in clubs page.

--- client/src/pages/clubs/clubs.jsx
+++ client/src/pages/clubs/clubs.jsx
@@ -23,21 +23,20 @@ function ClubsPage() {
   useEffect(()=> {
     getClubs();
   }, [])
-  console.log(ClubsData);
+  
     return(
         <div>
             <AppNavBar/>
             <div>
-                <h1 style={{textAlign: "center", margin: "2em 0em 1em 0em"}}>Clubs Page </h1>
+                <h1 style={{textAlign: "center", margin: "2em 0em 1em 0em"}}>Super League Clubs</h1>
             </div>
-            <div className="clubs-body">
+            <div className="container">
             { ClubsData ?
                     (ClubsData.length > 0 ?
                       ClubsData.map((item) => {
-                        
                         return(
                           <div key={item.name}> 
-                            <ClubCard clubName = {item.name} >  </ClubCard>
+                            <ClubCard clubName = {item.name} asciName = {item.asci_name} >  </ClubCard>
                           </div>
                         );
                       }) : <></>)            :<></>
