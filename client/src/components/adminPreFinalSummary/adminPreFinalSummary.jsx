204ecc410e77bac14b90904c3684548ab8c4fa43
Huseyin Alper Karadeniz <halperkk@gmail.com>
2022-12-23 21:33:14 +0300

    Add admin update pre-week preFinalSummary
    Created final summary page for the pre-week update.

---
+++ client/src/components/adminPreFinalSummary/adminPreFinalSummary.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../adminRefAssign/adminRefAssign.css";
+import * as ReactBootstrap from "react-bootstrap";
+
+function AdminPreFinalSummaryPage ( {currentWeek, allData, formData, setFormData} ){
+
+    return(
+        <div>
+            <a>Pre-Week Final Summary Page</a>
+        </div>
+    )
+};
+
+export default AdminPreFinalSummaryPage;
