63c0ab6283894c9783a46e1e81c82b31fab56318
Huseyin Alper Karadeniz <halperkk@gmail.com>
2022-12-24 21:02:22 +0300

    Fix referee assignment page in admin pre-week
    Updated referee assignment before admin pre-week update.

--- client/src/components/refAssignBox/refAssignBox.jsx
+++ client/src/components/refAssignBox/refAssignBox.jsx
@@ -3,11 +3,13 @@ import "../refAssignBox/refAssignBox.css";
 
 function RefAssignBox({ refereeData, matchData, idx }) {
 
+    console.log(refereeData);
+
     return (
         <>
         <div className="ref-assign-box-container col-sm-12 col-md-6 col-lg-4">
             <div className="ref-assign-box-details">
-                <p><b>Referee {idx+1}:</b> {refereeData.name}</p>
+                <p><b>Referee {idx+1}:</b> {refereeData.name} ({refereeData.ratio})</p>
                 <p>{matchData.club1} vs. {matchData.club2}</p>
             </div>
         </div>
