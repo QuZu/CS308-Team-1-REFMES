e0937067ac3850c28b812fa8c2e65565673cf4f7
Enis Mert Kuzu <emertkuzu35@gmail.com>
2022-12-24 04:13:02 +0300

    Add new profile page

--- client/src/pages/profile/profile.jsx
+++ client/src/pages/profile/profile.jsx
@@ -1,41 +1,34 @@
-import { useStore } from "../../store/store";
 import "../profile/profile.css";
-import { useNavigate } from "react-router";
 import React, { useEffect, useState } from "react";
-import { userLogout } from "../../store/userreducer";
 import axios from "axios";
 import AppNavBar from "../../components/appnavbar.jsx";
 import stadium from "../profile/allclubs.png"
-// '../../stadiums/fenerbahce.jpg';
 import profilePhoto from '../../components/refbar/user_profile.png'
-import {BsTwitter, BsInstagram, BsLinkedin, BsPencilFill, BsStarFill, BsStarHalf, BsStar} from 'react-icons/bs';
-import CommentBox from "../../components/comment/commentbox";
-import findLogo from "../../components/clubLogos/clubLogos";
-import findSocialMedia from "../../components/clubLogos/clubsocials";
+import { useParams } from "react-router-dom";
+import ProfileComments from "./profile-comments";
+import { BsPencilFill} from 'react-icons/bs';
+import * as ReactBootstrap from "react-bootstrap";
+import { useStore } from "../../store/store"
 function ProfilePage() {
-    const [state, dispatch] = useStore();
-    const navigate = useNavigate();
-    const {user:currentUser} = state;
-    const [UserComments,setUserComments]=useState([]);
+    const params = useParams();
+    const currUsername = params.username;
     const [loading,setLoading]=useState(false);
-    const [errorMessage, setErrorMessage] = useState("");
-    var clubLogo=findLogo(currentUser.user.fan_of)
-    var clubSocialInfo=findSocialMedia(currentUser.user.fan_of)
-    const getUserComments = async () => {
-      await axios.get(`${process.env.REACT_APP_URL}/api/comments/getUserComments/${currentUser.user.id}`).then(res => {
-          if (res.data == []) {
-              console.log("Empty");
-          } else {
-              setUserComments(res.data);
-              setLoading(true);
-          }
-      }).catch(err => console.log(err))
-  };
-console.log(UserComments);
+    const [state, dispatch] = useStore();
+    const {user:currentUser} = state;
+    console.log(currentUser.user.username);
+    const [userData,setuserData]=useState();
+    const getCurrentUser=async()=>{
+      await axios.get(`${process.env.REACT_APP_URL}/api/users/getuserbyUsername/${currUsername}`).then(res=>{
+        setuserData(res.data);
+        setLoading(true);
+      }).catch(err=>{
+        console.log(err);
+      })
+    }
+    
 useEffect(() => {
-  getUserComments();
+  getCurrentUser();
 }, [])
-
  return(
   <div className="profile-info row align-items-center mr-0">
     <AppNavBar/>
@@ -47,62 +40,20 @@ useEffect(() => {
         </div>
         <div className="user_profile_headline">
             <img src={profilePhoto} alt="img"/> 
-            <article style={{display:"flex"}}><p style={{marginRight: "5px", fontSize: "30px"}}>{currentUser.user.full_name}</p><p style={{marginTop:"15px"}}>(@{currentUser.user.username})</p>
-            <div className="user_edit_pen"><a href="/edit"><BsPencilFill/></a></div>
+            <article style={{display:"flex"}}><p style={{marginRight: "5px", fontSize: "30px"}}>{userData.full_name}</p><p style={{marginTop:"15px"}}>(@{userData.username})</p>
+            {currentUser.user.username===currUsername ? 
+            <div className="user_edit_pen"><a href="/edit"><BsPencilFill/></a></div> :
+            <></>
+            }
             </article>
         </div>
       </div>
-      <div className="user_profile_main">
-        <div className="col-4 left-sidebar-margin">
-          <div className="profile-left-sidebar">
-            <p className="mt-2 profile-section-text d-flex justify-content-center">Fan Information</p>
-            <hr></hr>
-            <div className="club_container">
-              <div className="col-4 club_inner"><img src={clubLogo}/></div>
-              <div className="col-8 club_inner"><p>{currentUser.user.fan_of}</p></div>
-            </div>
-            <div className="social_media_container">
-              <div className="social_media_inner"><a style={{color: "#1D9BF0"}} href={clubSocialInfo.twitter}><BsTwitter/></a></div>
-              <div className="social_media_inner"><a style={{color: "#FE0088"}} href={clubSocialInfo.instagram}><BsInstagram/></a></div>
-            </div>
-            <p className="mt-2 profile-section-text d-flex justify-content-center">User Information</p>
-            <hr></hr>
-            <div className="user-profile-information">
-              <p className="d-flex justify-content-center profile-subsection-text">Email:</p>
-              <input className=" user-profile-input" disabled={true} value={currentUser.user.email}></input>
-              <p className="d-flex justify-content-center profile-subsection-text">Username:</p>
-              <input className=" user-profile-input" disabled={true} value={currentUser.user.username}></input>
-              <p className="d-flex justify-content-center profile-subsection-text">Full Name:</p>
-              <input className="user-profile-input" disabled={true} value={currentUser.user.full_name}></input>
-              <p className="d-flex justify-content-center profile-subsection-text">Total Comments:</p>
-              <p className="d-flex justify-content-center profile-section-text">{UserComments.length}</p>
-            </div> 
-          </div>
-        </div>
-          <div className="col-6">
-            <p  className="d-flex justify-content-center profile-section-text">All Comments</p>
-            <hr className="user-profile-divider"></hr>
-            <div className="user-profile-comment-scroll"> 
-              {UserComments.length > 0 ? UserComments.slice(0, 10).map((item) => {
-
-                return(
-                <div style={{width: "100%"}} key={item._id}>
-                    <div className="container d-flex justify-content-center">
-                    <CommentBox commentPerson={currentUser.user.full_name} pComment={item.comment} myDate={item.date} MatchData={item.match_infos}/>
-                    </div>
-                </div>
-
-                );
-                })
-                :
-                <p style={{marginTop: "1em"}}>No comments yet!</p>
-                }
-            </div>
-          </div>   
-      </div>
+      <ProfileComments CurrentUser={userData}/>
     </div>
     :
-    <div>Comments Loading...</div>
+    <div className="d-flex justify-content-center">
+    <ReactBootstrap.Spinner animation="border"/>
+    </div>
   }
   
   </div>
