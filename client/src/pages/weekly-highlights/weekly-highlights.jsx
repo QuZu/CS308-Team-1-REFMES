59f8e6fc4d37a3c8dbe4fafdb3afc6add95a3a20
mkarabulut23 <mehmeteren.360@hotmail.com>
2022-12-25 12:37:53 +0300

    Add Weekly Highlights Page

--- client/src/pages/weekly-highlights/weekly-highlights.jsx
+++ client/src/pages/weekly-highlights/weekly-highlights.jsx
@@ -1,14 +1,119 @@
 import { useStore } from "../../store/store";
-import AppNavBar from "../../components/appnavbar.jsx";
 import { useNavigate } from "react-router";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import axios from "axios";
+import RefHighlightCard from "../../components/refbar/refhighlightcard";
+function WHighlightsPage({currentWeek}) {
+    const [refereesOfWeek, setRefereesOfWeek] = useState([]);
+    const [loading,setLoading] = useState(false);
+    const getRefereesOfWeek = async() => {
+        await axios
+            .get(`${process.env.REACT_APP_URL}/api/refereesOfWeek/getRefereesOfWeek/${currentWeek}`) //o hafta görev yapan hakemleri çekmiş oldum refereesOfWeek arrayine
+            .then(res => {
+                setRefereesOfWeek(res.data.myarray);
+                setLoading(true);
+        }).catch(err => console.log(err));
+    };
 
-function WHighlightsPage() {
+    useEffect(() => {
+        getRefereesOfWeek();
+    }, []);
+
+    function compareObserverVotes( a, b) {
+        if ( a.observerRating[currentWeek][0]/a.observerRating[currentWeek][1] < b.observerRating[currentWeek][0]/b.observerRating[currentWeek][1]){
+          return -1;
+        }
+        if ( a.observerRating[currentWeek][0]/a.observerRating[currentWeek][1] > b.observerRating[currentWeek][0]/b.observerRating[currentWeek][1]){
+          return 1;
+        }
+        return 0;
+      }
+      function compareFanVotes( a, b) {
+        if ( a.postRating[currentWeek][0]/a.postRating[currentWeek][1] < b.postRating[currentWeek][0]/b.postRating[currentWeek][1]){
+          return -1;
+        }
+        if ( a.postRating[currentWeek][0]/a.postRating[currentWeek][1] > b.postRating[currentWeek][0]/b.postRating[currentWeek][1]){
+          return 1;
+        }
+        return 0;
+      }
+      const sortedObserverVotes =  refereesOfWeek.sort(compareObserverVotes);
+      const bestObserverVotes = sortedObserverVotes.slice(0,3);
+      const worstObserverVote = sortedObserverVotes.slice(8);
+      const sortedFanVotes = refereesOfWeek.sort(compareFanVotes);
+      const bestFanVotes = sortedFanVotes.slice(0,3);
+      const worstFanVotes = sortedFanVotes.slice(8);
     return(
         <div>
-            <AppNavBar/>
-            <h1 style={{textAlign: "center", marginTop: "172px"}}>Weekly Highlights Page</h1>
+            <div className="row">
+                <div className="col-5" style={{margin:"20px", marginLeft:"100px"}}>
+            {loading && refereesOfWeek ?
+                <div style={{marginTop: "20px"}}>
+                    <h4 style={{textAlign:"center", marginRight:"100px"}}>BEST REFEREES FOR OBSERVERS IN WEEK {currentWeek}</h4>
+                {bestObserverVotes.map((item) => {
+                    return(<div>
+                        <RefHighlightCard Refdata={item} r_username={item.r_username} Refname={item.name} week ={currentWeek}></RefHighlightCard>
+                        </div>)
+                })}
+                </div>
+                :
+                <div>
+                   <p>Loading...</p>
+                </div>
+            }
+                </div>
+                <div className="col-5" style={{margin:"20px"}}>
+             {loading && refereesOfWeek ?
+                <div style={{marginTop:"20px"}}>
+                    <h4 style={{textAlign:"center", marginRight:"100px"}}>BEST REFEREES FOR FANS IN WEEK {currentWeek}</h4>
+                {bestFanVotes.map((item) => {
+                    return(<div>
+                       <RefHighlightCard Refdata={item} r_username={item.r_username} Refname={item.name} week ={currentWeek}></RefHighlightCard>
+                        </div>)
+                })}
+                </div>
+                :
+                <div>
+                   <p>Loading...</p>
+                </div>
+            }
+                  </div>
+              </div>
+
+              <div className="row">
+                <div className="col-5" style={{margin:"20px", marginLeft:"100px"}}>
+            {loading && refereesOfWeek ?
+                <div style={{marginTop: "20px"}}>
+                    <h4 style={{textAlign:"center", marginRight:"100px"}}>WORST REFEREE FOR OBSERVERS IN WEEK {currentWeek}</h4>
+                {worstObserverVote.map((item) => {
+                    return(<div>
+                        <RefHighlightCard Refdata={item} r_username={item.r_username} Refname={item.name} week ={currentWeek}></RefHighlightCard>
+                        </div>)
+                })}
+                </div>
+                :
+                <div>
+                   <p>Loading...</p>
+                </div>
+            }
+                </div>
+                <div className="col-5" style={{margin:"20px"}}>
+             {loading && refereesOfWeek ?
+                <div style={{marginTop:"20px"}}>
+                    <h4 style={{textAlign:"center", marginRight:"100px"}}>WORST REFEREE FOR FANS IN WEEK {currentWeek}</h4>
+                {worstFanVotes.map((item) => {
+                    return(<div>
+                       <RefHighlightCard Refdata={item} r_username={item.r_username} Refname={item.name} week ={currentWeek}></RefHighlightCard>
+                        </div>)
+                })}
+                </div>
+                :
+                <div>
+                   <p>Loading...</p>
+                </div>
+            }
+                  </div>
+              </div>
         </div>
     )
 }
