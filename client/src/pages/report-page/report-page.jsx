360bcabea09cc53632ed610739cf676c0eedb1ea
Ahmet Bilal Yildiz <71377609+abilals58@users.noreply.github.com>
2022-12-27 22:30:33 +0300

    Add report icon to the appnavbar.

---
+++ client/src/pages/report-page/report-page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./report-page.css";
+
+
+function ReportPage(){
+
+
+    return(
+
+        <div>
+            <h1> Report Page</h1>
+        </div>
+    )
+
+
+}
+
+export default ReportPage
