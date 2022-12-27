3b2f6e4f4a060cab57090f8019bd7105acea899d
Ahmet Bilal Yildiz <71377609+abilals58@users.noreply.github.com>
2022-12-27 23:35:01 +0300

    Add form to report page.

--- client/src/pages/report-page/report-page.jsx
+++ client/src/pages/report-page/report-page.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import AppNavBar from "../../components/appnavbar.jsx";
 import "./report-page.css";
 
 
@@ -6,12 +7,26 @@ function ReportPage(){
 
 
     return(
+        <div className="fullscreen row">
+            <div className="report-page-container">
+            <AppNavBar/>
+            <div className="row report-page-header"> <h1> Report Page</h1> </div>
+            <div className="row report-page-form">
+                <form>
+                    <div class="form-group">
+                        <label className="report-page-label" for="exampleFormControlTextarea1">Please write your report and click send report.</label>
+                        <textarea class="report-page-text-area form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <button type="button" class="btn btn-success">Send report</button>
+                    </div>
 
-        <div>
-            <h1> Report Page</h1>
+                </form>
+
+
+            </div>
+            </div>
+            
         </div>
     )
-
 
 }
 
