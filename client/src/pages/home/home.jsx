6e15c46e899ed438d4cd64f81c1cf34a6da0f965
Huseyin Alper Karadeniz <halperkk@gmail.com>
2022-12-30 18:47:26 +0300

    Fix home page for the top standings
    Updated the home page to show top live standings of clubs.

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,17 +1,33 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { useStore } from "../../store/store";
 import AppNavBar from "../../components/appnavbar.jsx";
 import Carousel from 'react-bootstrap/Carousel';
+import * as ReactBootstrap from "react-bootstrap";
 
 function Home() {
    const [state, dispatch] = useStore();
    const {user: currentUser} = state;
-
+   const [standings, setStandings] = useState();
+   const [loading, setLoading] = useState(false);
    const [index, setIndex] = useState(0);
 
    const handleSelect = (selectedIndex, e) => {
      setIndex(selectedIndex);
    };
+
+   const getStandings = async()=>{
+      await axios.get(`${process.env.REACT_APP_URL}/api/matches/getstandings`).then(res=>{
+         setStandings(res.data);
+         console.log(res.data);
+         setLoading(true);
+      }).catch(err => console.log(err))
+   };
+
+   useEffect(() => {
+      getStandings();
+   }, [])
 
    return(
       <div>
@@ -19,30 +35,39 @@ function Home() {
          <Carousel activeIndex={index} onSelect={handleSelect}>
             <Carousel.Item style={{height: "600px", background: "#bc4"}}>
                <img className="d-block w-100 h-100"/>
-               <Carousel.Caption>
+               <Carousel.Caption style={{marginBottom: "1.5em"}}>
                   <h2>Welcome to REFMES!</h2>
                   <p>Referee Management and Assignment System for TFF</p>
                </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item style={{height: "600px", background: "#23d"}}>
                <img className="d-block w-100 h-100"/>
-               <Carousel.Caption>
+               <Carousel.Caption style={{marginBottom: "1.5em"}}>
                   <h2>Give Ratings to Referees</h2>
                   <p>Give both pre-match and post-match ratings</p>
                </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item style={{height: "600px", background: "#42a"}}>
                <img className="d-block w-100 h-100"/>
-               <Carousel.Caption>
+               <Carousel.Caption style={{marginBottom: "1.5em"}}>
                   <h2>Track Live Results</h2>
                   <p>Live status of referee assignments and matches</p>
                </Carousel.Caption>
             </Carousel.Item>
          </Carousel>
          <div>
-            <h1 style={{textAlign: "center", marginTop: "172px", fontSize: "42px"}}>WELCOME, THIS IS <b>REFMES</b>!</h1>
-            <h2 style={{textAlign: "center", marginTop: "72px", fontSize: "24px"}}>Justice in Football!</h2>
+            <h1 style={{textAlign: "center", marginTop: "132px", fontSize: "42px"}}>WELCOME, THIS IS <b>REFMES</b>!</h1>
+            <h2 style={{textAlign: "center", marginTop: "72px", marginBottom: "72px", fontSize: "24px"}}>Justice in Football!</h2>
          </div>
+         { loading ?
+            (standings.map((item, index) => {
+               return(<div key={index} idx={index}>{index+1} - {standings[index].team.name}</div>);
+            }))
+         :
+         <div className="d-flex justify-content-center">
+            <ReactBootstrap.Spinner animation="border"/>
+         </div>   
+         }
       </div>
    )
 }
